5460c74626d9c66ddd4387964685215fbab76e36
Shamiim Namulema <75389039+shamiim611@users.noreply.github.com>
2025-01-13 14:26:57 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,13 @@ import sklearn
 st.title('Machine learning app')
 
 st.info('This is an app for a machine learning model that predicts penguin body mass')
-df = pd.read_csv('https://raw.githubusercontent.com/shamiim611/penguin_body_mass/refs/heads/master/penguins_clean.csv')
-df
+with st.expander('Data'):
+  st.write('**raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/shamiim611/penguin_body_mass/refs/heads/master/penguins_clean.csv')
+  df
+  st.write('**X**')
+  X =df.drop(columns= 'Body Mass (g)', axis =1)
+  X
+  st.write('**y**')
+  y = df['Body Mass (g)']
+  y
