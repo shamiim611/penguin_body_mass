f7c13ad3623b5927f311a9949949d492460c2423
Shamiim Namulema <75389039+shamiim611@users.noreply.github.com>
2025-01-13 18:24:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,6 @@ st.info('This is an app for a machine learning model that predicts penguin body 
 with st.expander('Data'):
   st.write('**raw data**')
   df = pd.read_csv('https://github.com/shamiim611/penguin_body_mass/blob/master/penguins_clean.xls',
-                    on_bad_lines = 'skip')
+                    error_bad_lines = False)
   df
  
