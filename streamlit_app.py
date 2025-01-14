a624e0f0a4989a252b69999868af243afcb70de6
Shamiim Namulema <75389039+shamiim611@users.noreply.github.com>
2025-01-14 13:02:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ data = {'Species':Species,
                      'Sex':Sex,
                      'Delta 15 (o/oo)':Delta_15_N,
                      'Delta 13 (o/oo)':Delta_13_C}
-input_df = pd.DataFrame(data, index[0]
+input_df = pd.DataFrame(data, index[0])
 input_penguins = pd.concat([input_df, X], axis =0)
                      
                      
